d235d53c00e60559d5e4344343a50ef25c48d847
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-12-12 16:55:50 -0300

    add call to action

--- src/Containers/Possibility/Possibility.jsx
+++ src/Containers/Possibility/Possibility.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './Possibility.module.css';
+import Photo from '../../assets/img/possibility.png';
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <section
+      className={`${styles.possibility} section__padding`}
+      id="possibility"
+    >
+      <div className={styles.img}>
+        <img src={Photo} alt="Possibility" />
+      </div>
+      <div className={styles.content}>
+        <h4>Request Early Access to Get Started</h4>
+        <h2 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h2>
+        <p className={styles.desc}>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <p className={styles.cta}>Request Early Access to Get Started</p>
+      </div>
+    </section>
+  );
 };
 
 export default Possibility;
--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import styles from './CallToAction.module.css';
 
 const CallToAction = () => {
-  return <div>CallToAction</div>;
+  return (
+    <div className={styles.cta}>
+      <div className={styles.inner_cta}>
+        <div className={styles.content}>
+          <p>Request Early Access to Get Started</p>
+          <h3>Register today & start exploring the endless possiblities.</h3>
+        </div>
+          <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CallToAction;
